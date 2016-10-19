7e9c6b0a2fdb943e51c448f7ce299400ffb5690d
Faith Szeto <faithszeto1@gmail.com>
2016-10-19 19:39:54 +0000

    submit hw2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,9 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
   def initialize(param)
+    @data = param
+  end
+  def bar(string, dict)
+    return "#{string}#{@data}#{dict[:sat]}"
   end
 end
--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -8,21 +8,29 @@ class Person
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'nickname'
+    @nickname = @name.to_s[0, 4]
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    2016 - @age.to_i
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'introduce'
+    "#{name} is #{@age}"
   end
 
   def fib_number
     # YOUR IMPLMENTATION HERE
-    0
+    pre = 0
+    cur = 1
+    fib = 0
+    for i in 0..(@age.to_i-1)
+      fib = pre+cur
+      pre = cur
+      cur = fib
+    end
+    return pre
   end
 end
